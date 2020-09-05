4fd195cf7927d3f7881f636b673c9c09f5546c41
Celestine <864409320@qq.com>
2020-09-05 23:17:51 +0800

    sucessBertAverageTrec

--- BertTrec.py
+++ BertTrec.py
@@ -5,7 +5,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 import json
 trainSet = './data/trainSet.txt'
 contentEmb = './data/conEmb.json'
-tagEeb = './data/tagEmb.txt'
+tagEmb = './data/tagEmb.txt'
 userEmb = './data/userEmb.txt'
 
 
@@ -21,13 +21,14 @@ def average_user_tweet(user, train_df, con_emb_dict):
     i = 0
     train_df = train_df.groupby(['user_id'], as_index=False).agg({'content': lambda x: list(x)})
     # train_df.to_csv(userEmb, sep='\t', index=False)
-    for content in train_df['content'].loc[train_df['user_id'] == user]:
-        user_list += content_embedding(content, con_emb_dict)
-        i += 1
-    for j in range(768):
+    for content_list in train_df['content'].loc[train_df['user_id'] == user]:
+        for content in content_list:
+            user_list += content_embedding(content, con_emb_dict)
+            i += 1
+    for j in range(i):
         user_list[j] /= i
-    user_arr = np.array(user_list).reshape(1, -1)
-    print(user_arr)
+    user_arr = np.array(user_list).reshape(-1, 1)
+    # print(user_arr)
     return user_arr
 
 
@@ -38,14 +39,15 @@ def average_hashtag_tweet(hashtag, train_df, con_emb_dict):
     train_df['hashtag'] = train_df['content'].apply(Preproce.get_hashtag)
     train_df = train_df.explode('hashtag').groupby(['hashtag'], as_index=False).agg({'content': lambda x: list(x)})
     # print(train_df.iat[1, 1])
-    # train_df.to_csv(tagEeb, sep='\t', index=False)
-    for content in train_df['content'].loc[train_df['hashtag'] == hashtag]:
-        tag_list += content_embedding(content, con_emb_dict)
-        i += 1
-    for j in range(768):
+    # train_df.to_csv(tagEmb, sep='\t', index=False)
+    for content_list in train_df['content'].loc[train_df['hashtag'] == hashtag]:
+        for content in content_list:
+            tag_list += content_embedding(content, con_emb_dict)
+            i += 1
+    for j in range(i):
         tag_list[j] /= i
-        tag_arr = np.array(tag_list).reshape(1, -1)
-    print(tag_arr)
+    tag_arr = np.array(tag_list).reshape(-1, 1)
+    # print(tag_arr)
     return tag_arr
 
 
@@ -57,9 +59,19 @@ def cosine_similar(user, hashtag, train_df, con_emb_dict):
 
 
 def rank_hashtag(train, content_emb):
+    # fake con_emb_dict
     con_emb_dict = []
     train_df = pd.read_table(train)
-    cosine_similar('TalkingTeenage', '#teens', train_df, con_emb_dict)
+    user_set = set(train_df['user_id'].tolist())
+    train_df['hashtag'] = train_df['content'].apply(Preproce.get_hashtag)
+    tag_set = set(train_df['hashtag'].explode('hashtag').tolist())
+    for user in user_set:
+        print('user: '+user)
+        for tag in tag_set:
+            print('tag: '+str(tag))
+            if str(tag) != 'nan':
+                print('yes')
+                cosine_similar(user, tag, train_df, con_emb_dict)
 
 
 if __name__ == '__main__':
